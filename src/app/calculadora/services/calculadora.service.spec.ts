2c494cef6312d98e6284a072a31f571675b2fbce
Matheus Manhães <matheus.manhaes@ed.company>
2019-09-09 18:55:14 -0300

    Adiciona nav

--- src/app/calculadora/services/calculadora.service.spec.ts
+++ src/app/calculadora/services/calculadora.service.spec.ts
@@ -14,35 +14,35 @@ describe('CalculadoraService', () => {
 
   it('Deve garantir que 1 + 1 é igual a 2',
     inject([CalculadoraService], (service: CalculadoraService) => {
-      let soma = service.calcular(1, 1, CalculadoraService.SOMA);
+      const soma = service.calcular(1, 1, CalculadoraService.SOMA);
       expect(soma).toEqual(2);
     })
   );
 
   it('Deve garantir que 3 - 2 é igual a 1',
     inject([CalculadoraService], (service: CalculadoraService) => {
-      let subtracao = service.calcular(3, 2, CalculadoraService.SUBTRACAO);
+      const subtracao = service.calcular(3, 2, CalculadoraService.SUBTRACAO);
       expect(subtracao).toEqual(1);
     })
   );
 
   it('Deve garantir que 4 / 2 é igual a 2',
     inject([CalculadoraService], (service: CalculadoraService) => {
-      let divisao = service.calcular(4, 2, CalculadoraService.DIVISAO);
+      const divisao = service.calcular(4, 2, CalculadoraService.DIVISAO);
       expect(divisao).toEqual(2);
     })
   );
 
   it('Deve garantir que 2 * 2 é igual a 4',
     inject([CalculadoraService], (service: CalculadoraService) => {
-      let multiplicacao = service.calcular(2, 2, CalculadoraService.MULTIPLICACAO);
+      const multiplicacao = service.calcular(2, 2, CalculadoraService.MULTIPLICACAO);
       expect(multiplicacao).toEqual(4);
     })
   );
 
   it('Deve retornar 0 para operação inválida',
     inject([CalculadoraService], (service: CalculadoraService) => {
-      let multiplicacao = service.calcular(2, 2, '%');
+      const multiplicacao = service.calcular(2, 2, '%');
       expect(multiplicacao).toEqual(0);
     })
   );
